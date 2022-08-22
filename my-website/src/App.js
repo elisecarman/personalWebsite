f6d29793df4ca0abc143f5aef37b1bb23d1f0db9
elisecarman <77816390+elisecarman@users.noreply.github.com>
2022-08-21 17:02:46 -0700

    update

--- my-website/src/App.js
+++ my-website/src/App.js
@@ -5,6 +5,10 @@ import Nav from './components/Nav';
 import { Link , Outlet} from "react-router-dom";
 import { Text, Box, Flex, Spacer, Image} from '@chakra-ui/react';
 import classes from './components/general.module.css';
+import art1 from './assets/artwork1.png';
+import art3 from './assets/artwork3.png';
+import art4 from './assets/artwork4.png';
+import art5 from './assets/artwork5.png';
 import 'animate.css';
 
 function App() {
